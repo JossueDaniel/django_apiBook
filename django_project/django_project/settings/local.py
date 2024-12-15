4f07211b2473eaba369e399103306ab89f908101
danny <jossuedaniel@outlook.com>
2024-12-14 21:42:44 -0500

    Agregando el contenedor db para la base de datos

--- django_project/django_project/settings/local.py
+++ django_project/django_project/settings/local.py
@@ -1,10 +1,25 @@
+import dj_database_url
+
 from .base import *
+
 
 DEBUG = True
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('POSTGRES_DB'),
+        'USER': env.str('POSTGRES_USER'),
+        'PASSWORD': env.str('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
+
