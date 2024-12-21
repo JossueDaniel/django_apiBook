47a86cce9f1744454b4c585108a671836653ae7f
danny <jossuedaniel@outlook.com>
2024-12-20 20:57:46 -0500

    Arreglando storage whitenoise

--- django_project/django_project/settings/base.py
+++ django_project/django_project/settings/base.py
@@ -119,11 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STORAGES = {
-    'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage'
-    }
-}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
